7eb0774921d0e67265531253949c3bd7f4422340
devdavidias <davitdt206@gmail.com>
2026-01-20 19:00:13 -0300

    fix: update User model to include progress object

--- models/User.js
+++ models/User.js
@@ -1,9 +1,11 @@
-const mongoose = require ('mongoose')
+const mongoose = require('mongoose');
 
-const User = mongoose.model('User',{
-    name:String,
-    email:String,
-    password:String
-})
+const User = mongoose.model('User', {
+    name: String,
+    email: String,
+    password: String,
+    // Adicione esta linha abaixo para permitir salvar os dados das páginas
+    progress: { type: Object, default: {} }
+});
 
-module.exports = User ;
+module.exports = User;
